4e4f73208a8dcf009f06c66a7f034b00cca4611b
AirAmetyst <117623954+AirAmetyst@users.noreply.github.com>
2023-03-17 18:23:34 +1100

    Update Base.ino

--- UM(M)P/Base/Base.ino
+++ UM(M)P/Base/Base.ino
@@ -1,33 +1,27 @@
-#include <Servo.h>
+
 int IN1=8;
 int IN2=9;
 int IN3=7;
 int IN4=5;
 int ENA=10;
 int ENB=6;
-Servo highright;
-Servo highleft;
-Servo lowleft;
-Servo lowright;
+
 void setup()
 {
  for (int i = 5; i <11; i ++) 
    {
      pinMode(i, OUTPUT);   
    }
-  highright.attach(12);
-  lowright.attach(13);
-  highleft.attach(4);
-  lowright.attach(3);
+  
 }
 void loop()
 {
-  /*
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
+  
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2, LOW);
   analogWrite(ENA,255); 
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH); 
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW); 
   analogWrite(ENB,255); 
   delay(10000);
   analogWrite(ENA,0); 
@@ -43,8 +37,8 @@ void loop()
   analogWrite(ENA,0); 
   analogWrite(ENB,0); 
   delay(5000); 
-  */
-  lowleft.write(180);
-  lowleft.write(180);
+  
+  
+  
   
 }
